c1fb8e6fd6f51c4bf97775763e2626433b1ae021
Adarsh kumar <adarshkumar308855@gmail.com>
2022-12-19 03:15:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const buttons=document.querySelectorAll("button");
+const screen=document.getElementById('screen');
+
+
+for(let btn of buttons){
+    btn.addEventListener('click',(e) =>{
+        const btnText=btn.innerText;
+        
+
+        if(btnText==='C'){
+            screen.value="";
+        }
+
+        else if(btnText==='X'){
+            screen.value+='*'
+        }
+
+        else if(btnText==='='){
+
+            try{
+           screen.value= eval(screen.value);
+            }
+
+            catch(e){
+                screen.value='Invalid Operation'
+            }
+            
+        }
+
+        else{
+            screen.value+=btnText;
+        }
+
+    })
+
+}
